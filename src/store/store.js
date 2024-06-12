03cb95f147a20fc4ac8e6a85546a6a3561547501
pqthinh <phamquangquang2008@gmail.com>
2024-06-13 01:45:39 +0700

    fix store

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import bookReducer from './bookReducer';
+import { configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import bookReducer from './bookReducer'
 
 const persistConfig = {
   key: '@root',
   storage: AsyncStorage
-};
-const persistedReducer = persistReducer(persistConfig, bookReducer);
+}
+const persistedReducer = persistReducer(persistConfig, bookReducer)
 
 const store = configureStore({
   reducer: persistedReducer,
-});
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
 
-export default store;
-export const persistor = persistStore(store);
+export default store
+export const persistor = persistStore(store)
